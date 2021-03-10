4224a9f110b33ce3f816818fb390a7897828a59b
trident-kanda <tridentkandayuki@gmail.com>
2021-03-10 20:19:57 +0900

    test

--- pages/contents/[id].tsx
+++ pages/contents/[id].tsx
@@ -19,12 +19,11 @@ import css from "highlight.js/lib/languages/css";
 import html from "highlight.js/lib/languages/xml";
 import "highlight.js/styles/atom-one-dark.css";
 import { idparams, idprops } from "../../lib/tsutil";
-
-hljs.registerLanguage("javascript", javascript);
-hljs.registerLanguage("xml", html);
-hljs.registerLanguage("css", css);
 const Contents = ({ postData, lateStart, popularData }: idprops) => {
   useEffect(() => {
+    hljs.registerLanguage("javascript", javascript);
+    hljs.registerLanguage("xml", html);
+    hljs.registerLanguage("css", css);
     hljs.highlightAll();
     countdb(postData.id);
   }, []);
