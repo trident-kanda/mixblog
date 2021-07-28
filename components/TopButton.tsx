8b25404b68375e665e52dfdebd14cd9ec9c1ac8f
trident-kanda <tridentkandayuki@gmail.com>
2021-07-28 19:59:09 +0900

    topButton

--- components/TopButton.tsx
+++ components/TopButton.tsx
@@ -1,9 +1,14 @@
 import topButtonStyle from "../style/topButton.module.scss";
 import ArrowIcon from "./icon/arrowIcon";
-
+const returnTop = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
 const TopButton = () => {
   return (
-    <div className={topButtonStyle.circle}>
+    <div className={topButtonStyle.circle} onClick={returnTop}>
       <ArrowIcon />
     </div>
   );
