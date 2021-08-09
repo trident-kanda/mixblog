de66ab14d00dd1b833919d7d4365a9f3c1374871
trident-kanda <tridentkandayuki@gmail.com>
2021-08-09 16:47:53 +0900

    tag

---
+++ components/Tag.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import tagStyle from "../style/tag.module.scss";
+type props = {
+  tagList: string[];
+};
+const Tag = ({ tagList }: props) => {
+  return (
+    <div className={tagStyle.tag_container}>
+      {tagList.map((tag, num) => {
+        return (
+          <Link href={`tag/${tag}`}>
+            <a className={tagStyle.tag} key={num}>
+              {tag}
+            </a>
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Tag;
